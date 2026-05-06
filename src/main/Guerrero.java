ba164bb7d0bded2b5a13c79f56775c186fd8d640
leSifirus <sifirus157@gmail.com>
2026-05-06 00:27:31 -0300

    nueva clase uwu

---
+++ src/main/Guerrero.java
@@ -0,0 +1,8 @@
+package main;
+public class Guerrero extends Personaje {
+    
+    public Guerrero(int fuerza, int mana, int defensa, int agilidad, int vidaHP, int nivel, int exp) {
+        super(fuerza, mana, defensa, agilidad, vidaHP, nivel, exp);
+    }
+    
+}
