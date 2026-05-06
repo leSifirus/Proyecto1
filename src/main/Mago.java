f7d29b12e554f6b35f13a3f493184ac5bf66e30c
leSifirus <sifirus157@gmail.com>
2026-05-06 00:27:16 -0300

    nueva clase uwu

---
+++ src/main/Mago.java
@@ -0,0 +1,8 @@
+package main;
+public class Mago extends Personaje {
+    
+    public Mago(int fuerza, int mana, int defensa, int agilidad, int vidaHP, int nivel, int exp) {
+        super(fuerza, mana, defensa, agilidad, vidaHP, nivel, exp);
+    }
+    
+}
