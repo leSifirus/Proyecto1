a601180e9dcff6b0b5f2945aec8e6158c3520799
leSifirus <sifirus157@gmail.com>
2026-05-07 17:33:36 -0300

    main casi final

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,14 +2,14 @@ package main;
 import java.util.Scanner;
 import java.util.HashMap;  
 public class Main {
-     
+    static String correo;
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         HashMap<String, Usuario> usuarios = new HashMap<>();
+        HashMap<String, Personaje> personajes = new HashMap<>();
         int opcion;
         int opPersonaje;
-        String correo;
         String nombre = "";
         
         
@@ -33,6 +33,9 @@ public class Main {
                     correo = sc.nextLine();
                     System.out.println("Selecciona un fecha de nacimiento: ");
                     String FecNacimiento = sc.nextLine();
+                    if (usuarios.size() > 0 && usuarios.containsKey(correo)) {
+                        System.out.println("ERROR!! Este correo ya esta registrado");
+                    }
                     Usuario usuario = new Usuario(nombre, apellido, apodo, correo, FecNacimiento);
                     System.out.println("Tu clave es: " + usuario.getClave() + " |||GUARDALA|||");
 
@@ -44,12 +47,61 @@ public class Main {
                     correo = sc.nextLine();
                     System.out.println("Ingrese contraseña: ");
                     String clave = sc.nextLine();
-                    
                     if (usuarios.containsKey(correo)) {
                         Usuario usuarioRegistrado = usuarios.get(correo);
                         if (usuarioRegistrado.getClave().equals(clave)) {
                             System.out.println("INICIO DE SESION EXITOSO");
-                            opcion = 0;
+                             do {
+                                System.out.println("\n===BIENVENIDO " + nombre + "===");
+                                System.out.println("\n===MENU DE JUEGO===\n");
+                                System.out.println("1. Ver mis personajes");
+                                System.out.println("2. Crear personaje");
+                                System.out.println("3. Ver bodega de personajes");
+                                System.out.println("4. Ver perfil y estadisticas");
+                                System.out.println("0. Para salir");
+                                opcion = Integer.parseInt(sc.nextLine());
+
+                                switch (opcion){
+                                    case 1: 
+                                        System.out.println("===MIS PERSONAJES===");
+                                        break;
+                                    case 2:
+                                        System.out.println("===CREAR PERSONAJES===\n");
+                                        System.out.println("1. Guerrero");
+                                        System.out.println("2. Mago");
+                                        System.out.println("3. Arquero");
+                                        opPersonaje = Integer.parseInt(sc.nextLine());
+
+                                        System.out.println("Elige el nombre de tu personaje: ");
+                                        String nombrePersonaje = sc.nextLine();
+                                        switch (opPersonaje){
+                                            case 1: Guerrero guerrero = new Guerrero(70, 120, 50, 20, 1001,nombrePersonaje, 1, 0, "Activo");
+                                            personajes.put(correo, guerrero);
+                                            break;
+
+                                            case 2: Mago mago = new Mago(45, 280, 30, 30, 740,nombrePersonaje, 1, 0, "Activo");
+                                            personajes.put(correo, mago);
+                                            break;
+
+                                            case 3: Clerigo clerigo = new Clerigo(30, 250, 33, 28, 780,nombrePersonaje, 1, 0, "Activo");
+                                            personajes.put(correo, clerigo);
+                                            break;
+                                        }   
+                                        break;
+                                    case 3:
+                                        System.out.println("===BODEGA===");
+                                        break;
+                                    case 4:
+                                        System.out.println("===PERFIL===");
+                                        break;
+                                    case 0:
+                                        System.out.println("Adios");
+
+                                }
+            
+            
+            
+        } while (opcion != 0);
                         }
                         else {
                             System.out.println("Clave incorrecta");
@@ -62,48 +114,9 @@ public class Main {
             case 0: System.out.println("Hasta luego"); break;
             default: System.out.println("Opción inválida");
          }
+         
         } while (opcion != 0);
-        do {
-            System.out.println("\n===BIENVENIDO " + nombre + "===");
-            System.out.println("\n===MENU DE JUEGO===\n");
-            System.out.println("1. Ver mis personajes");
-            System.out.println("2. Crear personaje");
-            System.out.println("3. Ver bodega de personajes");
-            System.out.println("4. Ver perfil y estadisticas");
-            System.out.println("0. Para salir");
-            opcion = Integer.parseInt(sc.nextLine());
-            
-            switch (opcion){
-                case 1: 
-                    System.out.println("===MIS PERSONAJES===");
-                    break;
-                case 2:
-                    System.out.println("===CREAR PERSONAJES===\n");
-                    System.out.println("1. Guerrero");
-                    System.out.println("2. Mago");
-                    System.out.println("3. Arquero");
-                    opPersonaje = Integer.parseInt(sc.nextLine());
-                    
-                    System.out.println("Elige el nombre de tu personaje: ");
-                    String nombrePersonaje = sc.nextLine();
-                    switch (opPersonaje){
-                        case 1: Guerrero guerrero= new Guerrero(70, 120, 50, 20, 1001,nombrePersonaje, 1, 0, "Activo");
-                    }
-                    break;
-                case 3:
-                    System.out.println("===BODEGA===");
-                    break;
-                case 4:
-                    System.out.println("===PERFIL===");
-                    break;
-                case 0:
-                    System.out.println("Adios");
-                    
-            }
-            
-            
-            
-        } while (opcion != 0);
+       
         
         sc.close();
             }
