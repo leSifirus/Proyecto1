48159d0d63115bc1c428e4d21c4a8ea1cba1f421
leSifirus <sifirus157@gmail.com>
2026-05-07 15:07:00 -0300

    a

--- src/main/Personaje.java
+++ src/main/Personaje.java
@@ -4,21 +4,62 @@ public class Personaje {
    protected int mana;
    protected int defensa;
    protected int agilidad;
-   
    protected int vidaHP;
+   
+   protected String nombre;
    protected int nivel;
    protected int exp;
+   protected String estado;
 
-    public Personaje(int fuerza, int mana, int defensa, int agilidad, int vidaHP, int nivel, int exp) {
+    public Personaje(int fuerza, int mana, int defensa, int agilidad, int vidaHP, String nombre, int nivel, int exp, String estado) {
         this.fuerza = fuerza;
         this.mana = mana;
         this.defensa = defensa;
         this.agilidad = agilidad;
         this.vidaHP = vidaHP;
+        this.nombre = nombre;
         this.nivel = nivel;
         this.exp = exp;
+        this.estado = estado;
     }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public int getAgilidad() {
+        return agilidad;
+    }
+
+    public int getVidaHP() {
+        return vidaHP;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    
    
-   
+    
    
 }
