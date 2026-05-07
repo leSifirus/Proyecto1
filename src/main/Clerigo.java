dbbc0cb8987c6ba4a553c53a4390fa8657ff8e6c
leSifirus <sifirus157@gmail.com>
2026-05-07 17:32:55 -0300

    enum clase personaje

---
+++ src/main/Clerigo.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Clerigo extends Personaje{
+
+    public Clerigo(int fuerza, int mana, int defensa, int agilidad, int vidaHP, String nombre, int nivel, int exp, String estado) {
+        super(fuerza, mana, defensa, agilidad, vidaHP, nombre, nivel, exp, estado);
+    }
+    
+}
