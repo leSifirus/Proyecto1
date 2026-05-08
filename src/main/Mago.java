5d4b03586b9bc1106f98152393fb016981791638
leSifirus <sifirus157@gmail.com>
2026-05-07 21:18:58 -0300

    package

--- src/main/Mago.java
+++ src/main/Mago.java
@@ -1,4 +1,6 @@
 package main;
+import Personajes.Personaje;
+
 public class Mago extends Personaje {
 
     public Mago(int fuerza, int mana, int defensa, int agilidad, int vidaHP, String nombre, int nivel, int exp, String estado) {
