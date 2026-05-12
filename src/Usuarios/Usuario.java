769c41426c23beafe2e61cfd9a5176db4222b4db
Carlos <carloscubillangutierrez@gmail.com>
2026-05-11 23:03:41 -0400

    pene penesito like if you like cumsito uwwwu

--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class Usuario {
+public class Usuario {  
     private String nombre;
     private String apellido;
     private String apodo;
