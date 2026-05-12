ce4d327bf2877d6d4ad3bcd88029561843440665
Carlos <carloscubillangutierrez@gmail.com>
2026-05-11 23:14:33 -0400

    pene penesito like if you like cumsito uwwwu

--- src/Personajes/Clerigo.java
+++ src/Personajes/Clerigo.java
@@ -3,7 +3,7 @@ import Personajes.Personaje;
 
 public class Clerigo extends Personaje{
 
-    public Clerigo(int fuerza, int mana, int defensa, int agilidad, int vidaHP, String nombre, int nivel, int exp, String estado) {
+    public Clerigo(int fuerza, int mana, int defensa, int agilidad, int vidaHP, String nombre, int nivel, int exp, estadoPersonaje estado) {
         super(fuerza, mana, defensa, agilidad, vidaHP, nombre, nivel, exp, estado);
     }
     @Override
