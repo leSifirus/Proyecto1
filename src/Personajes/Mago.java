8eb5afa36a0982a61f812fb69931a3811032c83d
Carlos <carloscubillangutierrez@gmail.com>
2026-05-11 23:14:23 -0400

    pene penesito like if you like cumsito uwwwu

--- src/Personajes/Mago.java
+++ src/Personajes/Mago.java
@@ -3,7 +3,7 @@ import Personajes.Personaje;
 
 public class Mago extends Personaje {
 
-    public Mago(int fuerza, int mana, int defensa, int agilidad, int vidaHP, String nombre, int nivel, int exp, String estado) {
+    public Mago(int fuerza, int mana, int defensa, int agilidad, int vidaHP, String nombre, int nivel, int exp, estadoPersonaje estado) {
         super(fuerza, mana, defensa, agilidad, vidaHP, nombre, nivel, exp, estado);
     }
     @Override
