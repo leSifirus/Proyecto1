1a4229c53763c6ccf8f218ec7d7034ee8d95ddfd
leSifirus <sifirus157@gmail.com>
2026-05-12 01:26:45 -0300

    inventario

--- src/Inventario/Inventario.java
+++ src/Inventario/Inventario.java
@@ -3,51 +3,12 @@ package Inventario;
 import java.util.ArrayList;
 
 public class Inventario {
-    private ArrayList<Objeto> armas;
-    private ArrayList<Objeto> armaduras;
-    private ArrayList<Objeto> pociones;
-    private ArrayList<Objeto> miscelaneos;
+    private ArrayList<Objeto> objetos;
     private double limPeso;
 
-    public Inventario(ArrayList<Objeto> armas, ArrayList<Objeto> armaduras, ArrayList<Objeto> pociones, ArrayList<Objeto> miscelaneos, double limPeso) {
-        this.armas = armas;
-        this.armaduras = armaduras;
-        this.pociones = pociones;
-        this.miscelaneos = miscelaneos;
+    public Inventario(double limPeso) {
+        this.objetos = new ArrayList<>();
         this.limPeso = limPeso;
-        
-    }
-
-    public ArrayList<Objeto> getArmas() {
-        return armas;
-    }
-
-    public void setArmas(ArrayList<Objeto> armas) {
-        this.armas = armas;
-    }
-
-    public ArrayList<Objeto> getArmaduras() {
-        return armaduras;
-    }
-
-    public void setArmaduras(ArrayList<Objeto> armaduras) {
-        this.armaduras = armaduras;
-    }
-
-    public ArrayList<Objeto> getPociones() {
-        return pociones;
-    }
-
-    public void setPociones(ArrayList<Objeto> pociones) {
-        this.pociones = pociones;
-    }
-
-    public ArrayList<Objeto> getMiscelaneos() {
-        return miscelaneos;
-    }
-
-    public void setMiscelaneos(ArrayList<Objeto> miscelaneos) {
-        this.miscelaneos = miscelaneos;
     }
 
     public double getLimPeso() {
@@ -58,15 +19,15 @@ public class Inventario {
         this.limPeso = limPeso;
     }
     
-    public ArrayList<Objeto> getInventario() {
-        ArrayList<Objeto> completo = new ArrayList<>();
+    public void verInventarioCompleto() {
+        System.out.println("=== INVENTARIO COMPLETO ===");
+        if (objetos.isEmpty()) {
+            System.out.println("El inventario está vacío.");
+            return;
+        }
         
-        completo.addAll(this.armas);
-        completo.addAll(this.armaduras);
-        completo.addAll(this.pociones);
-        completo.addAll(this.miscelaneos);
-        
-        return completo;
-        
-    } 
+        for (Objeto obj : objetos) {
+            System.out.println("- " + obj.getNombre() + " (Tipo: " + obj.getTipo() + ", Peso: " + obj.getPeso() + ")");
+        }
+    }
 }
