b161435f6d56cd0c041aa06e8bcd60006168da04
Carlos <carloscubillangutierrez@gmail.com>
2026-05-11 23:03:13 -0400

    pene penesito like if you like cumsito uwwwu

--- src/Inventario/Inventario.java
+++ src/Inventario/Inventario.java
@@ -1,4 +1,72 @@
 package Inventario;
+
+import java.util.ArrayList;
+
 public class Inventario {
+    private ArrayList<Objeto> armas;
+    private ArrayList<Objeto> armaduras;
+    private ArrayList<Objeto> pociones;
+    private ArrayList<Objeto> miscelaneos;
+    private double limPeso;
+
+    public Inventario(ArrayList<Objeto> armas, ArrayList<Objeto> armaduras, ArrayList<Objeto> pociones, ArrayList<Objeto> miscelaneos, double limPeso) {
+        this.armas = armas;
+        this.armaduras = armaduras;
+        this.pociones = pociones;
+        this.miscelaneos = miscelaneos;
+        this.limPeso = limPeso;
+        
+    }
+
+    public ArrayList<Objeto> getArmas() {
+        return armas;
+    }
+
+    public void setArmas(ArrayList<Objeto> armas) {
+        this.armas = armas;
+    }
+
+    public ArrayList<Objeto> getArmaduras() {
+        return armaduras;
+    }
+
+    public void setArmaduras(ArrayList<Objeto> armaduras) {
+        this.armaduras = armaduras;
+    }
+
+    public ArrayList<Objeto> getPociones() {
+        return pociones;
+    }
+
+    public void setPociones(ArrayList<Objeto> pociones) {
+        this.pociones = pociones;
+    }
+
+    public ArrayList<Objeto> getMiscelaneos() {
+        return miscelaneos;
+    }
+
+    public void setMiscelaneos(ArrayList<Objeto> miscelaneos) {
+        this.miscelaneos = miscelaneos;
+    }
+
+    public double getLimPeso() {
+        return limPeso;
+    }
+
+    public void setLimPeso(double limPeso) {
+        this.limPeso = limPeso;
+    }
     
+    public ArrayList<Objeto> getInventario() {
+        ArrayList<Objeto> completo = new ArrayList<>();
+        
+        completo.addAll(this.armas);
+        completo.addAll(this.armaduras);
+        completo.addAll(this.pociones);
+        completo.addAll(this.miscelaneos);
+        
+        return completo;
+        
+    } 
 }
