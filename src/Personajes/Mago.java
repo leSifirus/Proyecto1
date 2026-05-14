fffe3654b74857342a3619b6f255dada19ea30e0
leSifirus <sifirus157@gmail.com>
2026-05-14 20:44:30 -0300

    mago

--- src/Personajes/Mago.java
+++ src/Personajes/Mago.java
@@ -1,14 +1,12 @@
 package Personajes;
-import Personajes.Personaje;
 
-public class Mago extends Personaje {
-
+public class Mago extends aDistancia {
+    
     public Mago(int fuerza, int mana, int defensa, int agilidad, int vidaHP, int oro, String nombre, int nivel, int exp, estadoPersonaje estado) {
-        super(fuerza, mana, defensa, agilidad, vidaHP,oro, nombre, nivel, exp, estado);
+        super(fuerza, mana, defensa, agilidad, vidaHP, oro, nombre, nivel, exp, estado);
     }
     @Override
     public String getClase(){
         return "Mago";
     }
-    
 }
