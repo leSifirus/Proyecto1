9cd72362590cbf416a14b14ee76b912a8f7c167e
leSifirus <sifirus157@gmail.com>
2026-05-17 21:51:17 -0300

    mision final

--- src/Misiones/Mision.java
+++ src/Misiones/Mision.java
@@ -25,19 +25,19 @@ public class Mision implements Serializable {
         this.estado = "DISPONIBLE";
     }
 
-    public final static ArrayList<Mision> crearCatalogo() {
-        ArrayList<Mision> catalogo = new ArrayList<>();
+    public final static Mision[] crearCatalogo() {
+        Mision[] catalogo = new Mision[10];
         
-        catalogo.add(new Mision("Derrotar slimes", "Derrota 5 slimes en el bosque", 1, 100, 100));
-        catalogo.add(new Mision("Recolectar hierbas", "Recolecta 10 hierbas medicinales", 2, 150, 150));
-        catalogo.add(new Mision("El lobo solitario", "Derrota al lobo alfa del valle", 3, 200, 250));
-        catalogo.add(new Mision("La cueva oscura", "Explora la cueva y encuentra el cristal", 4, 300, 350));
-        catalogo.add(new Mision("Proteger la aldea", "Defiende la aldea de los bandidos", 5, 400, 500));
-        catalogo.add(new Mision("El dragon dormido", "Despierta y derrota al dragon anciano", 6, 600, 650));
-        catalogo.add(new Mision("El tesoro perdido", "Encuentra el tesoro del rey pirata", 7, 500, 700));
-        catalogo.add(new Mision("La torre magica", "Llega a la cima de la torre magica", 8, 800, 850));
-        catalogo.add(new Mision("El ejercito oscuro", "Derrota al general del ejercito oscuro", 9, 900, 950));
-        catalogo.add(new Mision("El rey demonio", "Vence al rey demonio en combate final", 10, 1500, 1500));
+        catalogo[0] = new Mision("Derrotar slimes", "Derrota 5 slimes en el bosque", 1, 100, 100);
+        catalogo[1] = new Mision("Recolectar hierbas", "Recolecta 10 hierbas medicinales", 2, 150, 150);
+        catalogo[2] = new Mision("El lobo solitario", "Derrota al lobo alfa del valle", 3, 200, 250);
+        catalogo[3] = new Mision("La cueva oscura", "Explora la cueva y encuentra el cristal", 4, 300, 350);
+        catalogo[4] = new Mision("Proteger la aldea", "Defiende la aldea de los bandidos", 5, 400, 500);
+        catalogo[5] = new Mision("El dragon dormido", "Despierta y derrota al dragon anciano", 6, 600, 650);
+        catalogo[6] = new Mision("El tesoro perdido", "Encuentra el tesoro del rey pirata", 7, 500, 700);
+        catalogo[7] = new Mision("La torre magica", "Llega a la cima de la torre magica", 8, 800, 850);
+        catalogo[8] = new Mision("El ejercito oscuro", "Derrota al general del ejercito oscuro", 9, 900, 950);
+        catalogo[9] = new Mision("El rey demonio", "Vence al rey demonio en combate final", 10, 1500, 1500);
         
         return catalogo;
     }
